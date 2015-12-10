963e26c28e6cd3a3de702b89b37359d5b6e5424d
cph <cph@titan.r444b.ee.ntu.edu.tw>
2015-12-10 18:08:25 +0800

    add moedl.py

---
+++ model.py
@@ -0,0 +1,16 @@
+import sys
+
+import numpy as np 
+import sklean.ensemble import RandomForestClassifier
+
+train_X_fname = sys.argv[1]
+train_Y_fname = sys.argv[2]
+test_X_fname = sys.argv[3]
+test_pred_fname = sys.argv[4]
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
