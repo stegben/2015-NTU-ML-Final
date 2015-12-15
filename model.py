7a65cc39eec971f9d366744db9a25dd61735c7e2
cph <cph@titan.r444b.ee.ntu.edu.tw>
2015-12-15 16:16:43 +0800

    add data

--- model.py
+++ model.py
@@ -1,15 +1,38 @@
 import sys
 
 import numpy as np 
-import sklean.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import KFold
 
-train_X_fname = sys.argv[1]
-train_Y_fname = sys.argv[2]
-test_X_fname = sys.argv[3]
-test_pred_fname = sys.argv[4]
+train_fname = sys.argv[1]
+test_fname = sys.argv[2]
+output_fname = sys.argv[3]
+
+
+def read_data(fname, train_mode=True):
+    x = []
+    y = []
+    with open(fname, "r") as f:
+        f.readline()
+        for line in f:
+            data = line.rstrip().split(",")
+            data = [float(d) for d in data]
+            if train_mode:
+                x.append(data[1:-1])
+                y.append(data[-1])
+            else:
+                x.append(data[1:])
+    x = np.array(x)
+    y = np.array(y) if train_mode else None
+    return x, y
+
 
 def main():
-    
+    train_x, train_y = read_data()
+    print(X.values.shape)
+    print(Y.values.shape)
+
+    # clf = RandomForestClassifier(n_estimator=1000)
 
 
 if __name__ == "__main__":
